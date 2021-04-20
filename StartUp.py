93ad92491474eaee8090aaff8d94d2afce6ab2fb
sdev265ev <35968375+sdev265ev@users.noreply.github.com>
2021-04-20 14:22:51 -0500

    Update StartUp.py

--- StartUp.py
+++ StartUp.py
@@ -20,16 +20,9 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
 import socket
-import fcntl
-import struct
-
-def get_ip_address(ifname):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    return socket.inet_ntoa(fcntl.ioctl(
-        s.fileno(),0x8915,  # SIOCGIFADDR
-        struct.pack('256s', ifname[:15]))[20:24])
-
-get_ip_address('eth0')  # '192.168.0.110'
+Using socket:
+	import socket
+	print(socket.gethostbyname(socket.gethostname()))
 
 # Setup RPi device I/O.
 # The out pin will be pulled up if true is desired.
