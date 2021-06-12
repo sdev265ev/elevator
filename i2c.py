d7e9fc350c1109fd7b7a35f12e366d11aa35d033
sdev265ev <sdev265@zoomfrog.info>
2021-06-12 10:01:41 -0500

    Update i2c.py

--- i2c.py
+++ i2c.py
@@ -3,6 +3,40 @@ def main():
 	from smbus import SMBus
 	import time
 	
+	import wiringpi2 as wiringpi  
+	
+	 
+	pin_base = 65       # lowest available starting number is 65  
+	i2c_addr = 0x20     # A0, A1, A2 pins all wired to GND  
+
+	wiringpi.wiringPiSetup()                    # initialise wiringpi  
+	wiringpi.mcp23017Setup(pin_base,i2c_addr)   # set up the pins and i2c address  
+
+	wiringpi.pinMode(65, 1)         # sets GPA0 to output  
+	wiringpi.digitalWrite(65, 0)    # sets GPA0 to 0 (0V, off)  
+
+	wiringpi.pinMode(80, 0)         # sets GPB7 to input  
+	wiringpi.pullUpDnControl(80, 2) # set internal pull-up   
+
+	# Note: MCP23017 has no internal pull-down, so I used pull-up and inverted  
+	# the button reading logic with a "not"  
+
+	try:  
+	    while True:  
+		if not wiringpi.digitalRead(80): # inverted the logic as using pull-up  
+		    wiringpi.digitalWrite(65, 1) # sets port GPA1 to 1 (3V3, on)  
+		else:  
+		    wiringpi.digitalWrite(65, 0) # sets port GPA1 to 0 (0V, off)  
+		sleep(0.05)  
+	finally:  
+	    wiringpi.digitalWrite(65, 0) # sets port GPA1 to 0 (0V, off)  
+	    wiringpi.pinMode(65, 0)      # sets GPIO GPA1 back to input Mode  
+	    # GPB7 is already an input, so no need to change anything  
+	
+	
+	
+	
+	
 	# Define registers values from datasheet
 	IODIRA = 0x00  # IO direction A - 1= input 0 = output
 	IODIRB = 0x01  # IO direction B - 1= input 0 = output    
